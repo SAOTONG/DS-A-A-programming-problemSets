c21692cb0d96724a27b0fc1c5eb11ba584313e0d
SAO <1053001374@qq.com>
2019-06-05 15:55:47 +0800

    完善了函数名和变量名的命名规范

--- 第一章程序练习/程序19-P28/源.cpp
+++ 第一章程序练习/程序19-P28/源.cpp
@@ -1,60 +1,91 @@
+//该文件包括28页19、20、21题的代码实现
+//主要对递归函数和非递归函数的实现
+//发现在有数学公式的问题上使用递归函数比较容易
+//而编写非递归函数则需要观察并分析计算规律进行解决
 #include<iostream>
 #include<string>
 #include<exception>
 using namespace std;
-//非递归程序计算n!,从n到2
-int factorial_one(int n);
-//非递归程序计算n!,从2到n
-int factorial_two(int n);
-//递归函数计算斐波那契数Fn
-int facT1(int n);
-//非递归函数计算斐波那契数
-int facT2(int n);
+
+//非递归程序计算n!,从n到2,p28页19题
+int non_recursive_factorial_one(int n);
+//非递归程序计算n!,从2到n,p28页19题
+int non_recursive_factorial_two(int n);
+//递归函数计算斐波那契数Fn,p28页20题
+int recursive_fibonacci(int n);
+//非递归函数计算斐波那契数,p28页20题
+int non_recursive_fibonacci(int n);
+//递归函数计算p28页21题
+int recursive_page28_21(int n);
+//非递归函数计算p28页21题
+int non_recursive_page28_21(int n);
 
 int main() 
 {
 	//用负数、0、1、大于1的数测试factorial_one函数。
 	//四个数可以做到语句覆盖和分支覆盖测试。
-	cout << factorial_one(-1) << endl;
-	cout << factorial_one(0) << endl;
-	cout << factorial_one(1) << endl;
-	cout << factorial_one(5) << endl;
+	cout << non_recursive_factorial_one(-1) << endl;
+	cout << non_recursive_factorial_one(0) << endl;
+	cout << non_recursive_factorial_one(1) << endl;
+	cout << non_recursive_factorial_one(5) << endl;
 	cout << "-------------------\n";
 	//用负数、0、1、大于1的数测试factorial_two函数。
 	//四个数可以做到语句覆盖和分支覆盖测试。
-	cout << factorial_two(-1) << endl;
-	cout << factorial_two(0) << endl;
-	cout << factorial_two(1) << endl;
-	cout << factorial_two(5) << endl;
+	cout << non_recursive_factorial_two(-1) << endl;
+	cout << non_recursive_factorial_two(0) << endl;
+	cout << non_recursive_factorial_two(1) << endl;
+	cout << non_recursive_factorial_two(5) << endl;
 	cout << "-------------------\n";
 	try
 	{
 		//用0、1、2、大于2和负数测试facT1函数
-		cout << facT1(1) << endl;
-		cout << facT1(2) << endl;
-		cout << facT1(3) << endl;
-		cout << facT1(4) << endl;
-		cout << facT1(5) << endl;
+		//五个数可以做到语句覆盖和分支覆盖测试。
+		cout << recursive_fibonacci(0) << endl;
+		cout << recursive_fibonacci(1) << endl;
+		cout << recursive_fibonacci(2) << endl;
+		cout << recursive_fibonacci(3) << endl;
+		cout << recursive_fibonacci(-1) << endl;
 		cout << "-------------------\n";
 		//用0、1、2、大于2和负数测试facT2函数
-		cout << facT2(1) << endl;
-		cout << facT2(2) << endl;
-		cout << facT2(3) << endl;
-		cout << facT2(4) << endl;
-		cout << facT2(5) << endl;
+		//五个数可以做到语句覆盖和分支覆盖测试。
+		cout << non_recursive_fibonacci(0) << endl;
+		cout << non_recursive_fibonacci(1) << endl;
+		cout << non_recursive_fibonacci(2) << endl;
+		cout << non_recursive_fibonacci(3) << endl;
+		cout << non_recursive_fibonacci(-1) << endl;
+		cout << "-------------------\n";
+		//使用这个几个参数测试可以做到语句覆盖和分支覆盖测试。
+		cout << recursive_page28_21(0) << endl;
+		cout << recursive_page28_21(2) << endl;
+		cout << recursive_page28_21(4) << endl;
+		cout << recursive_page28_21(1) << endl;
+		cout << recursive_page28_21(3) << endl;
+		cout << recursive_page28_21(5) << endl;
+		cout << recursive_page28_21(-1) << endl;
+		cout << "-------------------\n";
+		//使用这个几个参数测试可以做到语句覆盖和分支覆盖测试。
+		cout << non_recursive_page28_21(0) << endl;
+		cout << non_recursive_page28_21(2) << endl;
+		cout << non_recursive_page28_21(4) << endl;
+		cout << non_recursive_page28_21(1) << endl;
+		cout << non_recursive_page28_21(3) << endl;
+		cout << non_recursive_page28_21(5) << endl;
+		cout << non_recursive_page28_21(-1) << endl;
+		cout << "-------------------\n";
 		system("pause");
 		return 0;
 	}
 	catch (const exception &e)
 	{
-		cerr << e.what() << endl;
+		cout << "程序运行出错！！\n";
+		cerr << "错误信息为:" << e.what() << endl;
 		system("pause");
 		return 1;
 	}
 	
 }
 
-int factorial_one(int n)
+int non_recursive_factorial_one(int n)
 {
 	if (n <= 1)
 		return 1;
@@ -68,7 +99,7 @@ int factorial_one(int n)
 
 }
 
-int factorial_two(int n)
+int non_recursive_factorial_two(int n)
 {
 	if (n <= 1)
 		return 1;
@@ -80,17 +111,17 @@ int factorial_two(int n)
 	return result;
 }
 
-int facT1(int n)
+int recursive_fibonacci(int n)
 {
 	if (n==1||n==2)
 		return 1;
 	else if (n > 2)
-		return facT1(n - 1) + facT1(n - 2);
+		return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2);
 	else
 		throw exception("斐波那契数不能小于1");
 }
 
-int facT2(int n)
+int non_recursive_fibonacci(int n)
 {
 	if (n == 1 || n == 2)
 		return 1;
@@ -110,4 +141,34 @@ int facT2(int n)
 	else
 		throw exception("斐波那契数不能小于1");
 	
+}
+
+//如果输入负数则抛出异常，如果输入小数则发生截断。
+int recursive_page28_21(int n)
+{
+	if (n >= 0)
+	{
+		if (n % 2 == 0)
+			return n / 2;
+		else
+			return recursive_page28_21(3 * n + 1);
+	}
+	else
+		throw exception("参数不能为负数");
+}
+
+int non_recursive_page28_21(int n)
+{
+	if (n >= 0)
+	{
+		while (true)
+		{
+			if (n % 2 == 0)
+				return n / 2;
+			else
+				n = 3 * n + 1;
+		}
+	}
+	else
+		throw exception("参数不能为负数");
 }
